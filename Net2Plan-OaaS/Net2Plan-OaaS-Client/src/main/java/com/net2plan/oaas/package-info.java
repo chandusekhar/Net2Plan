7942d7e4945008de3ed9f5bab915c3a682da35da
César <cesarfsannicolasmartinez@gmail.com>
2018-11-27 12:28:21 +0100

    Problems with javadoc included dependencies

---
+++ Net2Plan-OaaS/Net2Plan-OaaS-Client/src/main/java/com/net2plan/oaas/package-info.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Pablo Pavon Marino and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the 2-clause BSD License 
+ * which accompanies this distribution, and is available at
+ * https://opensource.org/licenses/BSD-2-Clause
+ *
+ * Contributors:
+ *     Pablo Pavon Marino and others - initial API and implementation
+ *******************************************************************************/
+
+/**
+ * Provides a set of classes for the OaaS client.
+ *
+ * @author Cesar San-Nicolas
+ * @since 0.7.0
+ */
+
+package com.net2plan.oaas;
