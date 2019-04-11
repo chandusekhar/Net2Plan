1c13be3ae06645b4f6ff58a7642a9626f672f3e0
DESKTOP-NO40SUS\Manuel <manu.hernandez.bastida@gmail.com>
2019-04-11 15:25:49 +0200

    FileManager --> Read/Write persistence file for OaaS AlgorithmPersistence -->Facilitate the user the construction of a correct object of persistence for the results of their algorithms. In the main method there is an example of use

--- Net2Plan-OaaS/Net2Plan-OaaS-Server/src/main/java/com/net2plan/oaas/FileManager.java
+++ Net2Plan-OaaS/Net2Plan-OaaS-Server/src/main/java/com/net2plan/oaas/FileManager.java
@@ -1,30 +1,23 @@
 package com.net2plan.oaas;
 
-import com.shc.easyjson.JSON;
-import com.shc.easyjson.JSONArray;
-import com.shc.easyjson.JSONObject;
-import com.shc.easyjson.JSONValue;
+import com.shc.easyjson.*;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.*;
 
 public class FileManager {
 
-
-    private Map<String,Object> persistenceData = new HashMap();
-    private JSONObject persistenceDataJSON = new JSONObject();
-
-    public JSONObject writePersistenceFile (String data, String algorithmName, String userToken) {
+    public static JSONObject writePersistenceFile (String data, String algorithmName, String userToken)
+    {
 
         JSONObject response = new JSONObject();
 
         try  {
 
             FileUtils.writeByteArrayToFile(new File(ServerUtils.TOMCAT_FILES_DIR+ File.separator + algorithmName + "_persistenceFile_"+userToken+".n2p"), "".getBytes());
-            FileUtils.writeByteArrayToFile(new File(ServerUtils.TOMCAT_FILES_DIR+ File.separator + algorithmName + "_persistenceFile.n2p"), data.getBytes());
+            FileUtils.writeByteArrayToFile(new File(ServerUtils.TOMCAT_FILES_DIR+ File.separator + algorithmName + "_persistenceFile_"+userToken+".n2p"), data.getBytes());
             response.put("status",new JSONValue("OK"));
             response.put("content",new JSONValue("No content for this function"));
         }catch(Exception ex){
@@ -34,8 +27,7 @@ public class FileManager {
         }
         return response;
     }
-
-    public JSONObject readPersistenceFile (String algorithmName, String userToken)
+    public static JSONObject readPersistenceFile (String algorithmName, String userToken)
     {
         JSONObject response = new JSONObject();
 
@@ -48,126 +40,11 @@ public class FileManager {
             response.put("content", new JSONValue(jsonObject));
 
         } catch (Exception ex) {
+            response.put("status", new JSONValue("ERROR"));
+            response.put("content", new JSONValue(ex.getMessage()));
             ex.printStackTrace();
-            return null;
         }
         return response;
     }
 
-    public void addPersistenceData(String key, Object value){
-
-        this.persistenceData.put(key,value);
-
-        /*if(value instanceof Map){
-            JSONObject mapObject = new JSONObject();
-            Map<String,Object> newMap = (Map<String, Object>)value;
-
-            for (Map.Entry<String, Object> entry : newMap.entrySet()) {
-                System.out.println("clave=" + entry.getKey() + ", valor=" + entry.getValue());
-                String newKey = entry.getKey();
-                Object object = entry.getValue();
-
-                if (value instanceof String) {
-                    mapObject.put(newKey, new JSONValue((String) object));
-                } else if (value instanceof Integer) {
-                    mapObject.put(newKey, new JSONValue((Integer) object));
-                } else if (value instanceof Double) {
-                    mapObject.put(newKey, new JSONValue((Double) object));
-                } else if (value instanceof Long) {
-                    mapObject.put(newKey, new JSONValue((Long) object));
-                }
-            }
-            persistenceDataJSON.put(key,new JSONValue(mapObject));
-        }else{
-            if(value instanceof String){
-                persistenceDataJSON.put(key,new JSONValue((String)value));
-            }else if(value instanceof Integer){
-                persistenceDataJSON.put(key,new JSONValue((Integer)value));
-            }else if(value instanceof Double){
-                persistenceDataJSON.put(key,new JSONValue((Double)value));
-            }else if(value instanceof Long){
-                persistenceDataJSON.put(key,new JSONValue((Long)value));
-            }
-
-        }
-
-        System.out.println(persistenceDataJSON);*/
-    }
-
-
-    public void write(){
-
-        persistenceDataJSON = new JSONObject();
-
-        for (Map.Entry<String, Object> entry : persistenceData.entrySet()) {
-            System.out.println("clave=" + entry.getKey() + ", valor=" + entry.getValue());
-            String newKey = entry.getKey();
-            Object value = entry.getValue();
-
-            if (value instanceof Collections){
-                JSONArray jsonArray = addCollectionObject(value);
-                if(jsonArray!=null)
-                    persistenceDataJSON.put(newKey,new JSONValue(jsonArray));
-
-            }else {
-                JSONValue jsonValue= addNoCollectionObject(value);
-                if(jsonValue != null)
-                    persistenceDataJSON.put(newKey,jsonValue);
-            }
-
-        }
-
-    }
-
-    public JSONValue addNoCollectionObject(Object value){
-
-        JSONValue jsonValue = null;
-
-        if (value instanceof String) {
-            jsonValue = new JSONValue((String) value);
-        } else if (value instanceof Integer) {
-            jsonValue = new JSONValue((Integer) value);
-        } else if (value instanceof Double) {
-            jsonValue = new JSONValue((Double) value);
-        } else if (value instanceof Long) {
-            jsonValue = new JSONValue((Long) value);
-        }
-        return jsonValue;
-    }
-
-    public JSONArray addCollectionObject(Object value){
-
-        JSONArray jsonArray = null;
-
-        if(value instanceof List){
-
-
-        }else if(value instanceof Map){
-
-        }
-
-        return jsonArray;
-
-    }
-
-
-
-
-
-    public static void main(String [] args){
-
-      FileManager fileManager = new FileManager();
-      fileManager.addPersistenceData("latencia",20.0);
-      fileManager.addPersistenceData("origen",2);
-
-      Map<String,Object> map = new HashMap<>();
-      List<Integer> numbers = new ArrayList();
-      map.put("id",1);
-      map.put("id",2);
-      map.put("id",3);
-
-      fileManager.addPersistenceData("links",map);
-      fileManager.addPersistenceData("destino",3);
-
-    }
 }
